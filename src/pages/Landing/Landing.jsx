73c7c049655f1208892597d06d7a27f164d3cd4c
MailenKleinman <maylenusca@gmail.com>
2023-10-17 16:21:05 -0300

    arregulé los textos

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -66,7 +66,7 @@ const Landing = (props) => {
                         <li><a href="#first" className="button big wide smooth-scroll-middle bold mainbtn">Jugar</a></li>
                     </ul>
                 </div>
-                <div className="itemfloat image">
+                <div className="  image">
                     <img src={votodibujo1} alt="votodibujo1" />
                 </div>
             </section>
@@ -75,17 +75,17 @@ const Landing = (props) => {
                 <div className="content back-in-right">
                     <h2 className='bold' >¿Cómo funciona Prodix?</h2>
                     <ul className="actions stacked">
-                        <li><p><span className='subtitle'>1. Registrate o Iniciá sesión</span> <br /> Abre la aplicación e inicia sesión con tu cuenta si ya tienes una, o regístrate si eres un nuevo usuario.</p></li>
-                        <li><p><span className='subtitle'>2. Hacé tu pronóstico</span> <br /> Selecciona tu elección de candidatos para cada categoría electoral, como presidente, gobernador, alcalde, etc. Puedes hacerlo de manera anónima o con tu perfil registrado.</p></li>
-                        <li><p><span className='subtitle'>3. Explorá las estadísticas</span> <br /> Accede a la sección de "Estadísticas en Tiempo Real" para ver cómo se están inclinando las predicciones de otros usuarios. Esto te dará una idea del sentimiento popular.</p></li>
-                        <li><p><span className='subtitle'>4. Compartí y participá por un premio</span> <br /> Continúa utilizando la aplicación para hacer seguimiento de las tendencias electorales y participar activamente en la comunidad electoral.</p></li>
+                        <li><p><span className='subtitle'>1. Registrate o Iniciá sesión</span> <br /> Abrí la aplicación e iniciá sesión con tu cuenta si ya tienes una, o registrate si sos un nuevo usuario.</p></li>
+                        <li><p><span className='subtitle'>2. Hacé tu pronóstico</span> <br /> Seleccioná tu predicción de cantidad de votos para cada candidato. Podés hacerlo de manera anónima o con tu perfil registrado.</p></li>
+                        <li><p><span className='subtitle'>3. Explorá las estadísticas</span> <br /> Guardá tu predicción y mirá las predicciones de otros usuarios. Esto te dará una idea del sentimiento popular.</p></li>
+                        <li><p><span className='subtitle'>4. Compartí y participá por un premio</span> <br /> Continuá utilizando la aplicación para hacer seguimiento de las tendencias electorales y participar activamente en estas elecciones.</p></li>
                        
                     </ul>
                      <ul className="actions stacked">
                         <li><a href="#" className="mainbtn button bold wide">Jugar</a></li>
                     </ul>
                 </div>
-                <div className="image itemfloat">
+                <div className="image  ">
                     <img src={votodibujo2} alt="" />
                 </div>
             </section>
@@ -104,7 +104,7 @@ const Landing = (props) => {
                         <li><a href="#" className="mainbtn button bold wide">Jugar</a></li>
                     </ul>
                 </div>
-                <div className="image itemfloat">
+                <div className="image  ">
                     <img src={votodibujo3} alt="" />
                 </div>
             </section>
@@ -113,7 +113,7 @@ const Landing = (props) => {
                 <div className="content">
                     <ul className="actions stacked back-in-right">
                         <li><p><span className='subtitle'>Hacé tu pronóstico</span> <br /> Con tu predicción de cuantos puntos va a tener cada candidato participás por un premio!</p></li>
-                        <li><p><span className='subtitle'>Estadísticas en tiempo real</span> <br /> Con tu predicción de cuantos puntos va a tener cada candidato participás por un premio!</p></li>
+                        <li><p><span className='subtitle'>Estadísticas en tiempo real</span> <br /> Mirá las predicciones de otras personas y enterate de qué piensan los argentinos</p></li>
                         <li><p><span className='subtitle'>Información de las elecciones</span> <br /> Conoce más sobre quiénes se postulan y cómo funciona el proceso democrático</p></li>
                         <li><p><span className='subtitle'>Foros de discusión</span> <br /> Participa en debates saludables y bien moderados con otros ciudadanos</p></li>
                         <li><p><span className='subtitle'>Seguro y anónimo</span> <br /> Tus datos están seguros y tu participación puede ser anónima.</p></li>
@@ -123,16 +123,16 @@ const Landing = (props) => {
                         <li><a href="#" className="mainbtn button bold wide">Jugar</a></li>
                     </ul>
                 </div>
-                <div className="content itemfloat">
+                <div className="content  ">
                     <img src={votodibujo4} alt="" style={{ width: '80%', margin:'3em' }} />
                 </div>
             </section>
 
             <section className="section-mt fadeIn wrapper style1 align-center">
                 <div className="inner">
-                    <h2 className='bold' >¿Quieres ser parte del pulso democrático de tu país?</h2>
+                    <h2 className='bold' >¿Querés ser parte del pulso democrático de tu país?</h2>
                     <p className='subtitle'>Sumá tu predicción a Prodix</p>
-                    <div className="image itemfloat" style={{display:'flex',justifyContent:'center'}}>
+                    <div className="image  " style={{display:'flex',justifyContent:'center'}}>
                         <img src={votodibujo5} alt="" style={{ width: '95%' }} />
                     </div>
                     <ul className="actions stacked">
